6ad395ead999afb98ee1c8cc42666e20c92718a7
Luiz galluzzi <luizgalluzzi@yahoo.com.br>
2022-10-21 09:34:38 -0300

    criado pasta assets, alterado posicionamento aviso

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -9,17 +9,17 @@ function islampbroken () {
 }
 function lampOn () {
     if (!islampbroken () ) {
-    lamp.src = "./Img/acesa.jpg";
+    lamp.src = "assets/Img/acesa.jpg";
     }
 }
 function lampOff () {
     if (!islampbroken () ) {
-    lamp.src = "./Img/apagada.jpg";
+    lamp.src = "assets/Img/apagada.jpg";
 }
 }
 function lampbroken () {
-    lamp.src = "./Img/quebrada.jpg";
-    aviso.src = "./Img/Avisado.jpg";
+    lamp.src = "assets/Img/quebrada.jpg";
+    aviso.src = "assets/Img/Avisado.jpg";
 }
 turnOn.addEventListener ("click", lampOn);
 turnOff.addEventListener ("click", lampOff);
